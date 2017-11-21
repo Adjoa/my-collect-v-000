b26ae110179ed73837aaacbdc6c87bd0e75b960f
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-21 17:28:09 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  if array = []
+    puts "This array is empty"
+  else
+    i = 0
+    new_array = []
+    while i < array.length do
+      new_array << yield(array[i])
+    end
+end
 
-
+my_collect(array) do |name|
+  name.split(" ").first
+end
